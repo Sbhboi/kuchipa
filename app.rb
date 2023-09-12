e7895650ecf6285a7b6697e3d505ea7120018bd0
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2023-09-12 11:34:44 +0800

    Server file to run the game

---
+++ app.rb
@@ -0,0 +1,40 @@
+require 'sinatra'
+
+def determine_winner(player_choice, computer_choice)
+    if player_choice == computer_choice
+        "Draw !"
+    elsif (player_choice == "Rock" && computer_choice == "Scissors") ||
+          (player_choice == "Paper" && computer_choice == "Rock") ||
+          (player_choice == "Scissors" && computer_choice == "Paper")
+        "You Win !"
+    else
+        "Computer.....Win !"
+    end
+end
+
+get '/' do
+    erb :index
+end
+
+post '/play' do
+    @player_name = params[:name]
+    @rounds = params[:rounds].to_i
+    erb :rounds
+end
+
+post '/result' do
+    @player_name = params[:name]
+    @player_choice = params[:choice]
+    @rounds = params[:rounds].to_i
+  
+    @results = []
+    @rounds.times do |round|
+        @player_choice = params["choice#{round}"]
+        @computer_choice = %w[Rock Paper Scissors].sample
+        result = determine_winner(@player_choice, @computer_choice)
+      @results << {round: round +1, player_choice: @player_choice, computer_choice: @computer_choice, result: result }
+    end
+
+    erb :result
+end
+
